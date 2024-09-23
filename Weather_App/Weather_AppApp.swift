6a7712fa3c269c1f6d591ef063cbb9e3e5f64a89
fazalpannu <fazalpannu22@gmail.com>
2024-09-23 10:35:10 +0500

    Initial Commit

---
+++ Weather_App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather_App
+//
+//  Created by Ml Bench on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
